931502d27b35962cdbbda9f81959215d29a9b105
ychy61 <ychy010601@gmail.com>
2024-09-20 21:00:31 +0900

    feat: Header 컴포넌트 추가

--- src/components/features/Layout/index.tsx
+++ src/components/features/Layout/index.tsx
@@ -1,8 +1,11 @@
 import styled from "@emotion/styled";
 import { Outlet } from "react-router-dom";
 
+import { Header } from "./Header";
+
 export const Layout = () => (
   <Wrapper>
+    <Header />
     <Outlet />
   </Wrapper>
 );
