539a8b47c2c3ade06c9d9d61ff7b0e9784f0adda
ychy61 <ychy010601@gmail.com>
2024-09-27 17:44:46 +0900

    feat: image 컴포넌트 추가

---
+++ src/components/features/Layout/common/Image/index.tsx
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+
+type Props = {
+  radius?: 'circle' | number;
+  ratio?: 'square' | 'auto' | number;
+} & React.ImgHTMLAttributes<HTMLImageElement>;
+
+export const Image = ({ ...props }: Props) => <Wrapper {...props} />;
+
+const Wrapper = styled.img<Pick<Props, 'ratio' | 'radius'>>(
+  {
+    objectFit: 'cover',
+    objectPosition: 'center',
+  },
+  ({ radius = 0 }) => {
+    if (radius === 'circle') {
+      return {
+        borderRadius: '50%',
+      };
+    }
+
+    if (typeof radius === 'number') {
+      return {
+        borderRadius: `${radius}px`,
+      };
+    }
+  },
+  ({ ratio = 'auto' }) => {
+    if (ratio === 'square') {
+      return {
+        aspectRatio: '1 / 1',
+      };
+    }
+
+    if (ratio === 'auto') {
+      return {
+        aspectRatio: 'auto',
+      };
+    }
+
+    if (typeof ratio === 'number') {
+      return {
+        aspectRatio: `${ratio}`,
+      };
+    }
+  },
+);
