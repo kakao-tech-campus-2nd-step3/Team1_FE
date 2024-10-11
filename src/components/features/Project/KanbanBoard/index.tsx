6029e3432861b5385bf32d02ee093654b279b99a
wjdsbs <wjdsbss@gmail.com>
2024-10-11 16:39:08 +0900

    refactor: 칸반보드의 initalTask를 별도의 파일로 이동

--- src/components/features/Project/KanbanBoard/index.tsx
+++ src/components/features/Project/KanbanBoard/index.tsx
@@ -18,6 +18,7 @@ import { useState } from 'react';
 
 import type { TaskStatus, TaskType } from '@/types/index';
 
+import { initialTasks } from './initialTask';
 import { KanbanColumn } from './KanbanColumn';
 
 interface Column {
@@ -26,87 +27,9 @@ interface Column {
 }
 
 export const KanbanBoard = () => {
-  const [tasks, setTasks] = useState<TaskType[]>([
-    {
-      id: 1,
-      name: '작업 1',
-      description: '작업 1 설명',
-      startDate: '2024-10-01T09:00:00Z',
-      endDate: '2024-10-15T17:00:00Z',
-      ownerId: 101,
-      progress: 5,
-      status: 'NOT_STARTED',
-      priority: 'HIGH',
-    },
-    {
-      id: 2,
-      name: '작업 2',
-      description: '작업 2 설명',
-      startDate: '2024-10-02T09:00:00Z',
-      endDate: '2024-10-16T17:00:00Z',
-      ownerId: 102,
-      progress: 3,
-      status: 'IN_PROGRESS',
-      priority: 'MEDIUM',
-    },
-    {
-      id: 3,
-      name: '작업 3',
-      description: '작업 3 설명',
-      startDate: '2024-10-03T09:00:00Z',
-      endDate: '2024-10-17T17:00:00Z',
-      ownerId: 103,
-      progress: 0,
-      status: 'NOT_STARTED',
-      priority: 'LOW',
-    },
-    {
-      id: 4,
-      name: '작업 4',
-      description: '작업 4 설명',
-      startDate: '2024-10-04T09:00:00Z',
-      endDate: '2024-10-18T17:00:00Z',
-      ownerId: 104,
-      progress: 50,
-      status: 'IN_PROGRESS',
-      priority: 'MEDIUM',
-    },
-    {
-      id: 5,
-      name: '작업 5',
-      description: '작업 5 설명',
-      startDate: '2024-10-05T09:00:00Z',
-      endDate: '2024-10-19T17:00:00Z',
-      ownerId: 105,
-      progress: 75,
-      status: 'IN_PROGRESS',
-      priority: 'HIGH',
-    },
-    {
-      id: 6,
-      name: '작업 6',
-      description: '작업 6 설명',
-      startDate: '2024-10-06T09:00:00Z',
-      endDate: '2024-10-20T17:00:00Z',
-      ownerId: 106,
-      progress: 100,
-      status: 'COMPLETED',
-      priority: 'LOW',
-    },
-    {
-      id: 7,
-      name: '작업 7',
-      description: '작업 7 설명',
-      startDate: '2024-10-07T09:00:00Z',
-      endDate: '2024-10-21T17:00:00Z',
-      ownerId: 107,
-      progress: 100,
-      status: 'COMPLETED',
-      priority: 'MEDIUM',
-    },
-  ]);
+  const [tasks, setTasks] = useState<TaskType[]>(initialTasks);
 
-  const columns: Column[] = [
+  const getColumns: Column[] = [
     {
       status: 'NOT_STARTED',
       tasks: tasks.filter((task) => task.status === 'NOT_STARTED'),
@@ -168,7 +91,7 @@ export const KanbanBoard = () => {
       >
         <SortableContext items={tasks.map((task) => task.id)}>
           <SimpleGrid columns={3} spacing={4} width="100%" p={4}>
-            {columns.map((column) => (
+            {getColumns.map((column) => (
               <KanbanColumn key={column.status} columns={column} />
             ))}
           </SimpleGrid>
