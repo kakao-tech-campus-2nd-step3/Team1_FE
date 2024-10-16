142f85e764a637d30e2c9c78c25b402f9b7c6c2a
wjdsbs <wjdsbss@gmail.com>
2024-10-16 18:54:35 +0900

    feat: 진행도 컴포넌트 구현 및 msw 설치

--- src/components/features/Project/ProgressTracker/index.tsx
+++ src/components/features/Project/ProgressTracker/index.tsx
@@ -1,3 +1,100 @@
+import { Avatar, Box, Flex, Progress, Stack, Text } from '@chakra-ui/react';
+
+import type { Role } from '@/types/index';
+
+const mockTeamProgress = [
+  {
+    teamMember: {
+      id: 101,
+      name: '춘식이',
+      role: 'developer' as Role,
+      imageURL: 'https://via.placeholder.com/40',
+    },
+    progress: 70,
+    activeTasks: [
+      {
+        id: 1,
+        name: '진행 중인 태스크 1',
+        progress: 30,
+        description: '설명',
+        startDate: '2024-10-01T09:00:00Z',
+        endDate: '2024-10-10T17:00:00Z',
+      },
+    ],
+  },
+  {
+    teamMember: {
+      id: 102,
+      name: '라이언',
+      role: 'developer' as Role,
+      imageURL: 'https://via.placeholder.com/40',
+    },
+    progress: 50,
+    activeTasks: [
+      {
+        id: 2,
+        name: '진행 중인 태스크 2',
+        progress: 30,
+        description: '설명',
+        startDate: '2024-10-15T09:00:00Z',
+        endDate: '2024-10-20T17:00:00Z',
+      },
+    ],
+  },
+];
+
+const teamProgress = mockTeamProgress;
+
 export const ProgressTracker = () => {
-  return <div>진행도 컴포넌트</div>;
+  return (
+    <Box
+      alignItems={'center'}
+      borderRadius={'10px'}
+      border={'1px solid #D8DADC'}
+      borderColor="#D8DADC"
+      p={8}
+    >
+      <Stack spacing={4} align="center">
+        {teamProgress.map((member) => (
+          <Flex
+            key={member.teamMember.id}
+            mt={2}
+            mb={2}
+            width="100%"
+            justifyContent="center"
+          >
+            <Flex alignItems="center" justifyContent="center">
+              <Avatar
+                name={member.teamMember.name}
+                src={member.teamMember.imageURL}
+                size="sm"
+              />
+              <Text fontWeight="bold" mr={4} width="80px" textAlign="center">
+                {member.teamMember.name}
+              </Text>
+            </Flex>
+            <Box flex="1" position="relative" overflow="hidden">
+              <Progress
+                value={member.progress}
+                colorScheme={'blue'}
+                height={8}
+                borderRadius="full"
+              />
+              <Text
+                position="absolute"
+                left={`${member.progress / 2}%`}
+                top="50%"
+                transform="translate(-50%, -50%)"
+                fontWeight="bold"
+                color="white"
+              >
+                {member.activeTasks.map((task) => task.name)}
+              </Text>
+            </Box>
+            <Text ml={4}>{member.progress}%</Text>
+          </Flex>
+        ))}
+      </Stack>
+    </Box>
+  );
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -12,3 +12,27 @@ export type TaskType = {
   status: TaskStatus;
   priority: TaskPriority;
 };
+
+export type Role = 'developer' | 'designer' | 'all';
+
+export type TeamMember = {
+  id: number;
+  name: string;
+  role: Role;
+  imageURL: string;
+};
+
+export type ActiveTask = {
+  id: number;
+  name: string;
+  progress: number;
+  description: string;
+  startDate: string;
+  endDate: string;
+};
+
+export type TeamProgress = {
+  teamMember: TeamMember;
+  progress: number;
+  activeTasks: ActiveTask[];
+};
